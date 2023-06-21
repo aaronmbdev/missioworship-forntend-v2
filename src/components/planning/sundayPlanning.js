d81ee0492cad91882a99a742fd84bc36e7902943
Aarón J. Morales Botton <aaron@midokura.com>
2023-06-22 00:02:34 +0200

    Esquema de planificación terminado

--- src/components/planning/sundayPlanning.js
+++ src/components/planning/sundayPlanning.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import MissioUtils from "../../service/utils";
 import AttendanceSheet from "../attendance/attendanceSheet";
+import AsyncSelect from "react-select/async";
 
 export default class SundayPlanning extends Component {
     render() {
@@ -18,7 +19,69 @@ export default class SundayPlanning extends Component {
                                 <AttendanceSheet showDate={false} day={extracted.day} month={extracted.month} year={extracted.year}/>
                             </div>
                             <div className="col-lg-6">
-
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <div className="form-group row mb-0">
+                                            <label className="col-md-4 col-form-label">Primera canción</label>
+                                            <div className="col-md-8">
+                                                <AsyncSelect
+                                                    isMulti
+                                                    cacheOptions
+                                                    defaultOptions
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row mt-3">
+                                    <div className="col-md-12">
+                                        <div className="form-group row mb-0">
+                                            <label className="col-md-4 col-form-label">Primera canción</label>
+                                            <div className="col-md-8">
+                                                <AsyncSelect
+                                                    isMulti
+                                                    cacheOptions
+                                                    defaultOptions
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row mt-3">
+                                    <div className="col-md-12">
+                                        <div className="form-group row mb-0">
+                                            <label className="col-md-4 col-form-label">Primera canción</label>
+                                            <div className="col-md-8">
+                                                <AsyncSelect
+                                                    isMulti
+                                                    cacheOptions
+                                                    defaultOptions
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row mt-3">
+                                    <div className="col-md-12">
+                                        <div className="form-group row mb-0">
+                                            <label className="col-md-4 col-form-label">Primera canción</label>
+                                            <div className="col-md-8">
+                                                <AsyncSelect
+                                                    isMulti
+                                                    cacheOptions
+                                                    defaultOptions
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row mt-4">
+                                    <div className="col-md-12">
+                                        <button type="button"
+                                                className="btn btn-primary btn-lg waves-effect waves-light">Guardar cambios
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
